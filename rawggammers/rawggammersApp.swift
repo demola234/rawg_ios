166769a21f2c69189eca77aa043e6fb45b917409
demola234 <kolawoleoluwasegun567@gmail.com>
2024-07-20 16:49:14 +0100

    Initial Commit

---
+++ rawggammers/rawggammersApp.swift
@@ -0,0 +1,17 @@
+//
+//  rawggammersApp.swift
+//  rawggammers
+//
+//  Created by Ademola Kolawole on 20/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct rawggammersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
